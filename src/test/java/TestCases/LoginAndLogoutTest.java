b1b55dc720cb8cc6205138aefdf9c8132c6b2a85
pikjian <bijian90@hotmail.com>
2024-10-26 22:39:50 +0800

    Try Jenkins Webhook

--- src/test/java/TestCases/LoginAndLogoutTest.java
+++ src/test/java/TestCases/LoginAndLogoutTest.java
@@ -4,7 +4,7 @@ import Pages.*;
 import org.testng.annotations.Test;
 
 public class LoginAndLogoutTest extends BaseTest{
-    //Commented due to avoid Test Failed from Maven Test Hi2
+    //Commented due to avoid Test Failed from Maven Test
 //    @Test
 //    public void testLogin() throws InterruptedException {
 //        PhoneNumberPage phoneNumberPage = loginPage.goToPhoneNumberPage();
